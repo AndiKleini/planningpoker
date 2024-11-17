e67799bafe87573de7c104e90ebf46e5b7fb029c
AndiKleini <Andi.Kleinbichler@gmail.com>
2024-11-17 14:15:00 +0100

    refactor: add test for hanlding multiple estimations

--- tests/handlertests.c
+++ tests/handlertests.c
@@ -6,16 +6,26 @@
 #include <string.h>
 #include "../headers/handler.h"
 
-static void process_request_of_estimation_and_query(void **state) {
+static void process_single_request_of_estimation_and_query(void **state) {
     char reqest[] = "ESTIMATE\nITEM1\n12\0";
     assert_string_equal(process_request(reqest)->msg, "OK\0");
     char reqquery[] = "GETRESULT\nITEM1\0";
     assert_string_equal(process_request(reqquery)->msg, "12\n\0");
 }
 
+static void process_multiple_requests_of_estimation_and_query(void **state) {
+    char reqest[] = "ESTIMATE\nITEM1\n12\0";
+    assert_string_equal(process_request(reqest)->msg, "OK\0");
+    char reqest2nd[] = "ESTIMATE\nITEM1\n23\0";
+    assert_string_equal(process_request(reqest2nd)->msg, "OK\0");
+    char reqquery[] = "GETRESULT\nITEM1\0";
+    assert_string_equal(process_request(reqquery)->msg, "12\n23\n\0");
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(process_request_of_estimation_and_query)
+        cmocka_unit_test(process_single_request_of_estimation_and_query),
+        cmocka_unit_test(process_multiple_requests_of_estimation_and_query)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
