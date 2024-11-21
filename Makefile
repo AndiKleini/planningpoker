634d9f97fd3a8f68c84f2389c8079d75514f3876
AndiKleini <Andi.Kleinbichler@gmail.com>
2024-11-21 20:33:59 +0100

    feature: read from sqlite database

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ build:
 	cc -Wall -o planningpokerserver.out planningpokerserver.c handler.c storage.c -lsqlite3 -lm
 
 test: build testhandler
-	cc -Wall -o planningpokerservertests.out ./tests/planningpokerservertests.c handler.c storage.c -lcmocka -lm
+	cc -Wall -o planningpokerservertests.out ./tests/planningpokerservertests.c handler.c storage.c -lcmocka -lm -lsqlite3
 	./planningpokerserver.out &
 	./planningpokerservertests.out || true
 	pkill planningpokerse
 
 testhandler:
-	cc -Wall -o handlertests.out ./tests/handlertests.c handler.c storage.c -lcmocka -lm -Wl,--wrap=store_estimation,--wrap=get_estimations
+	cc -Wall -o handlertests.out ./tests/handlertests.c handler.c storage.c -lcmocka -lsqlite3 -lm -Wl,--wrap=store_estimation,--wrap=get_estimations
 	./handlertests.out
 
 sqliteint:
--- storage.c
+++ storage.c
@@ -2,25 +2,76 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <sqlite3.h>
 
 char *ESTIMATION;
 
 int store_estimation(char *itemId, int value) 
 {
-    if (ESTIMATION == NULL) {
-        ESTIMATION = malloc(sizeof(char));
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc = sqlite3_open("./database/planningpoker.db", &db);
+    if(rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
     }
-    char *tmp = malloc(strlen(ESTIMATION)*sizeof(char)+ceil(log10(value))*sizeof(char)+sizeof('\n'));
-    sprintf(tmp, "%s%d\n", ESTIMATION, value);
-    free(ESTIMATION);
-    ESTIMATION = tmp;
+
+    char sqlins[54+strlen(itemId)+(int)ceil(log10(value))]; 
+    sprintf(sqlins, "INSERT INTO ESTIMATION (ITEMID,VALUE) VALUES('%s',%d);", itemId, value);
+
+    rc = sqlite3_exec(db, sqlins, NULL, 0, &zErrMsg);
+    if( rc != SQLITE_OK ){
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Records created successfully\n");
+    }
+
+    sqlite3_close(db);
     return 0;
 }
 
 char* get_estimations(char *itemId) 
-{
-    char *tmp = malloc(strlen(ESTIMATION)*sizeof(char));
-    strcpy(tmp, ESTIMATION);
-    memset(ESTIMATION, '\0', strlen(ESTIMATION));
-    return tmp;
+{ 
+    sqlite3 *db;
+    int rc = sqlite3_open("./database/planningpoker.db", &db);
+    if(rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    int sqlen = 46 + strlen(itemId);
+    char sqlsel[sqlen]; 
+    sprintf(sqlsel, "SELECT VALUE from ESTIMATION where ITEMID='%s';", itemId);
+    printf("%s\n", sqlsel);
+    sqlite3_stmt *stmt;
+    rc = sqlite3_prepare_v2(db, sqlsel, sqlen, &stmt, NULL);
+    if( rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+
+    rc = sqlite3_step(stmt);
+    const char *tmpret;
+    int retsize;
+    if (rc == SQLITE_ROW) {
+        tmpret = (char *)sqlite3_column_text(stmt, 0);
+        retsize = sqlite3_column_bytes(stmt, 0);
+    } else {
+        tmpret = "ERR";
+    }
+        
+    char *ret = malloc((retsize+1)*sizeof(char));
+    strcpy(ret, tmpret);
+    ret[retsize] = '\0';
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+
+    return ret;
 }
