73fc2056331607f40d8ad122946bf6a6acf30bbf
AndiKleini <Andi.Kleinbichler@gmail.com>
2024-12-08 21:54:26 +0100

    refactor: support multiple tests in testing script

--- tests/testplanningserver.sh
+++ tests/testplanningserver.sh
@@ -3,10 +3,13 @@
 SERVER=$1
 PORT=$2
 
+declare -i GLOBAL_RET=0
+
 estimate_and_query() 
 {
     ESTIMATED_VALUE=$1
     ITEMID=$2
+    EXPECTED_RES=$3
     response=$(printf "ESTIMATE\n$ITEMID\n$ESTIMATED_VALUE\0" | netcat -W 1 $SERVER $PORT)
     echo $response
     if [ "$response" != "OK" ]; then
@@ -16,12 +19,15 @@ estimate_and_query()
 
     response=$(printf "GETRESULT\n$ITEMID\0" | netcat -W 1 $SERVER $PORT)
     echo $response
-    if [ "$response" = "$ESTIMATED_VALUE" ]; then
-        return 0;
+    ret=0
+    if [ "$response" = "$EXPECTED_RES" ]; then
+        ret=0
     else 
-        echo "Did not receive expected value $ESTIMATED_VALUE of estimations. Intead got $response. \n"
-        return 1;
+        echo "Did not receive expected value $EXPECTED_RES of estimations. Intead got $response. \n"
+        ret=1
     fi
+    GLOBAL_RET=$(($GLOBAL_RET + $ret));
+    return $ret
 }
 
 echo "Testing for open socket on localhost on port $PORT !"
@@ -38,5 +44,9 @@ else
     return 1;
 fi
 
-estimate_and_query 10 ITEM1
-estimate_and_query 4 ITEM2
+estimate_and_query 10 ITEM1 10
+estimate_and_query 4 ITEM2 4
+estimate_and_query 56 ITEM3 56
+estimate_and_query 67 ITEM4 67
+estimate_and_query 78 ITEM5 78
+return $GLOBAL_RET
